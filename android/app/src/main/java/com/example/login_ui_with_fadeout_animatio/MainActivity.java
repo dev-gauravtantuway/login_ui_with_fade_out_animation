b0bfd17df57f666bfcbf48d1bf828f7647973fa1
Gaurav Tantuway <tantuwaygaurav@gmail.com>
2021-08-07 15:04:15 +0530

    Add files via upload

---
+++ android/app/src/main/java/com/example/login_ui_with_fadeout_animatio/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.login_ui_with_fadeout_animatio;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,256 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/services.dart';
+import 'dart:ui';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login UI with fadeOut animation',
+      home: HomePage(),
+    ),
+  );
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacity;
+  late Animation<double> _transform;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    );
+
+    _opacity = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.ease,
+      ),
+    )..addListener(() {
+        setState(() {});
+      });
+
+    _transform = Tween<double>(begin: 2, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.fastLinearToSlowEaseIn,
+      ),
+    );
+
+    _controller.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        brightness: Brightness.dark,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: ScrollConfiguration(
+        behavior: MyBehavior(),
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: size.height,
+            child: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xffFEC37B),
+                    Color(0xffFF4184),
+                  ],
+                ),
+              ),
+              child: Opacity(
+                opacity: _opacity.value,
+                child: Transform.scale(
+                  scale: _transform.value,
+                  child: Container(
+                    width: size.width * .9,
+                    height: size.width * 1.1,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(.1),
+                          blurRadius: 90,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(),
+                        Text(
+                          'Sign In',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black.withOpacity(.7),
+                          ),
+                        ),
+                        SizedBox(),
+                        component1(Icons.account_circle_outlined,
+                            'User name...', false, false),
+                        component1(
+                            Icons.email_outlined, 'Email...', false, true),
+                        component1(
+                            Icons.lock_outline, 'Password...', true, false),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            component2(
+                              'LOGIN',
+                              2.6,
+                              () {
+                                HapticFeedback.lightImpact();
+                                Fluttertoast.showToast(
+                                    msg: 'Login button pressed');
+                              },
+                            ),
+                            SizedBox(width: size.width / 25),
+                            Container(
+                              width: size.width / 2.6,
+                              alignment: Alignment.center,
+                              child: RichText(
+                                text: TextSpan(
+                                  text: 'Forgotten password!',
+                                  style: TextStyle(color: Colors.blueAccent),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                      Fluttertoast.showToast(
+                                        msg:
+                                            'Forgotten password! button pressed',
+                                      );
+                                    },
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(),
+                        RichText(
+                          text: TextSpan(
+                            text: 'Create a new Account',
+                            style: TextStyle(
+                              color: Colors.blueAccent,
+                              fontSize: 15,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Fluttertoast.showToast(
+                                  msg: 'Create a new Account button pressed',
+                                );
+                              },
+                          ),
+                        ),
+                        SizedBox(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget component1(
+      IconData icon, String hintText, bool isPassword, bool isEmail) {
+    double _width = MediaQuery.of(context).size.width;
+    return Container(
+      height: _width / 8,
+      width: _width / 1.22,
+      alignment: Alignment.center,
+      padding: EdgeInsets.only(right: _width / 30),
+      decoration: BoxDecoration(
+        color: Colors.black.withOpacity(.05),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextField(
+        style: TextStyle(color: Colors.black.withOpacity(.8)),
+        // cursorColor: Colors.white,
+        obscureText: isPassword,
+        keyboardType: isEmail ? TextInputType.emailAddress : TextInputType.text,
+        decoration: InputDecoration(
+          prefixIcon: Icon(
+            icon,
+            color: Colors.black.withOpacity(.7),
+          ),
+          border: InputBorder.none,
+          hintMaxLines: 1,
+          hintText: hintText,
+          hintStyle:
+              TextStyle(fontSize: 14, color: Colors.black.withOpacity(.5)),
+        ),
+      ),
+    );
+  }
+
+  Widget component2(String string, double width, VoidCallback voidCallback) {
+    double _width = MediaQuery.of(context).size.width;
+    return InkWell(
+      highlightColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      onTap: voidCallback,
+      child: Container(
+        height: _width / 8,
+        width: _width / width,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Color(0xff4796FF),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          string,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+        ),
+      ),
+    );
+  }
+}
+
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+    BuildContext context,
+    Widget child,
+    AxisDirection axisDirection,
+  ) {
+    return child;
+  }
+}
